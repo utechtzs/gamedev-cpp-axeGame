e8c67d1b3078fe3da64b77fd262ac344fb5dc1b7
Zechariah Utecht <utechtzs@gmail.com>
2024-06-03 15:05:58 -0500

    move circle with reset

--- main.cpp
+++ main.cpp
@@ -2,15 +2,45 @@
 
 int main()
 {
-	int x = 350;
-	int y = 200;
-	InitWindow(x, y, "My Window");
+	// window dimensions
+	int width = 350;
+	int height = 200;
+	InitWindow(width, height, "My Window");
 
+	// circle dimensions
+	int circle_x = width / 2;
+	int circle_y = height / 2;
+
+	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(WHITE);
-		DrawCircle(x / 2, y / 2, 25, BLUE);
+
+		DrawCircle(circle_x, circle_y, 25, BLUE);
+
+		if (IsKeyDown(KEY_D))
+		{
+			circle_x += 10;
+		}
+		else if (IsKeyDown(KEY_A))
+		{
+			circle_x -= 10;
+		}
+		else if (IsKeyDown(KEY_W))
+		{
+			circle_y -= 10;
+		}
+		else if (IsKeyDown(KEY_S))
+		{
+			circle_y += 10;
+		}
+		else if (IsKeyDown(KEY_SPACE))
+		{
+			circle_x = width / 2;
+			circle_y = height / 2;
+		}
+
 		EndDrawing();
 	}
 }
